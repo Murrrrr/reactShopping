e03f7af2c8e8dd717c8b09c4d27468e42177d921
Murrrrr <41497682+Murrrrr@users.noreply.github.com>
2022-03-08 10:39:13 +0900

    change for v4

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  var data = "안녕하세요?";
+  var className = "nav";
+
   return (
     <div className="App">
-      asdfasdf
+      <div className="black-nav">
+        <div>개발 blog</div>
+      </div>
+
+      <div className="content_list">
+        <ul>
+          <li>첫번째 글</li>
+          <li>두번째 글</li>
+          <li>세번째 글</li>
+        </ul>
+      </div>
     </div>
   );
 }
