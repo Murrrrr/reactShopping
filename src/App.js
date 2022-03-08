c1bfd71307acb16e7c10a3f6f27e247ae20eed09
Murrrrr <41497682+Murrrrr@users.noreply.github.com>
2022-03-08 10:15:49 +0900

    changes for v3

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>asdfasdf</h1>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>Hello from V2</h1>
+      </header>
     </div>
   );
 }
