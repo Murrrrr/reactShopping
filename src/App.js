412c48e1f0e66a24c2d0185fde5381d715d2ab00
Murrrrr <41497682+Murrrrr@users.noreply.github.com>
2022-03-13 18:44:49 +0900

    ver1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
+import React, { useState } from 'react';
+import logo from './logo.svg';
 import './App.css';
 
-function App() {
+function App(){
+  let posts = '강남 고기 맛집';
 
-  var data = "안녕하세요?";
-  var className = "nav";
+  let [글제목, 글제목변경] = useState(['남자 코트 추천', '강남 우동 맛집', '바지 맛집 추천']);
 
-  return (
+  return(
     <div className="App">
       <div className="black-nav">
-        <div>개발 blog</div>
+        <div>개발 Blog</div>
       </div>
 
-      <div className="content_list">
-        <ul>
-          <li>첫번째 글</li>
-          <li>두번째 글</li>
-          <li>세번째 글</li>
-        </ul>
+      <div className='list'>
+        <h3>{ 글제목[0] }</h3>
+        <p>2월 17일 발행</p>
+        <hr/>
+      </div>
+
+      <div className='list'>
+        <h3>{ 글제목[1] }</h3>
+        <p>2월 17일 발행</p>
+        <hr/>
+      </div>
+
+      <div className='list'>
+        <h3>{ 글제목[2] }</h3>
+        <p>2월 17일 발행</p>
+        <hr/>
       </div>
     </div>
-  );
+  )
 }
 
 export default App;
