5f8dcfce50d249f33ad6895dba21a7409621e9f1
Murrrrr <41497682+Murrrrr@users.noreply.github.com>
2022-03-16 12:50:32 +0900

    ver
    1

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App(){
   let posts = '강남 고기 맛집';
 
   let [글제목, 글제목변경] = useState(['남자 코트 추천', '강남 우동 맛집', '바지 맛집 추천']);
+  let [누른제목, 누른제목변경] = useState(0);
 
   return(
     <div className="App">
@@ -13,23 +14,32 @@ function App(){
         <div>개발 Blog</div>
       </div>
 
-      <div className='list'>
-        <h3>{ 글제목[0] }</h3>
-        <p>2월 17일 발행</p>
-        <hr/>
-      </div>
+      <button onClick={()=>{ 누른제목변경(0) }}>버튼1</button>
+      <button onClick={()=>{ 누른제목변경(1) }}>버튼2</button>
+      <button onClick={()=>{ 누른제목변경(2) }}>버튼3</button>
 
-      <div className='list'>
-        <h3>{ 글제목[1] }</h3>
-        <p>2월 17일 발행</p>
-        <hr/>
-      </div>
+      {글제목.map(function(a, idx){
+        return(
+          <div className='list'>
+            <h3 onClick={ ()=>{누른제목변경(idx)} }>{a}<span>?</span></h3>
+            <p>2월 18일 발행</p>
+            <hr />
+          </div>
+        )
+          
+        })
+      }
+      <Modal 글제목={글제목} 누른제목={누른제목}/>
+    </div>
+  )
+}
 
-      <div className='list'>
-        <h3>{ 글제목[2] }</h3>
-        <p>2월 17일 발행</p>
-        <hr/>
-      </div>
+function Modal(props){
+  return(
+    <div className='modal'>
+      <h2>제목{props.글제목[props.누른제목]}</h2>
+      <p>날짜</p>
+      <p>상세내용</p>
     </div>
   )
 }
